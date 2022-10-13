fdb3ba573647398c2b84d90c1322f270d7e2bcc7
Kwanele <kwanele097@gmail.com>
2022-10-13 22:25:21 +0200

    version2

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/pages/home.dart';
+import 'package:navigation/pages/about.dart';
+import 'package:navigation/pages/contact.dart';
+void main() => runApp( MaterialApp(
+ initialRoute: '/contact',
+ routes: {
+    '/':(context)=>const About(),
+   '/home': (context)=>const Home(),
+   '/contact': (context)=>const Contact(),
+ },
+));
+
---
+++ lib/pages/home.dart
@@ -0,0 +1,28 @@
+ import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+   const Home({Key? key}) : super(key: key);
+ 
+   @override
+   State<Home> createState() => _HomeState();
+ }
+ 
+ class _HomeState extends State<Home> {
+   @override
+   Widget build(BuildContext context) {
+     return   Scaffold(
+       backgroundColor: Colors.pink,
+       body: SafeArea(child: Column(
+         children: <Widget>[
+           TextButton(style:ButtonStyle(
+             foregroundColor: MaterialStateProperty.all( Colors.pink )
+           ), onPressed: (){
+             Navigator.pushNamed(context, '/contact');
+     },
+     child: const Text("my contact"),)
+         ],
+       ))
+     );
+   }
+ }
+ 
